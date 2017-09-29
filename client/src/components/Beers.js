1aac627005b120c09623a4a324a37ad5a924c015
Patticus3rd <patrick.seiuli@gmail.com>
2017-09-29 17:36:52 -0600

    added beers component

---
+++ client/src/components/Beers.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+import { Header } from 'semantic-ui-react';
+
+
+
+
+class Beers extends Component {
+    render() {
+      return (
+          <Header >
+              List of the Top 50 Beer:
+          </Header>
+        
+        
+      );
+    }
+  }
+  
+
+  export default Beers;
